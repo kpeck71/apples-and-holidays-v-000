b6b0197431a081f8f715db7c8dbdd39c3fd10fa3
Kristen Peck <kpeck71@gmail.com>
2017-09-26 12:55:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts seasons.to_s.capitalize! + ":"
     holidays.each do |holiday, supplies|
-      puts "   " + holiday.to_s.split(/ |\_/).map(&:capitalize).join(" ") + ": " + supplies.join(", ")
+      puts "  " + holiday.to_s.split(/ |\_/).map(&:capitalize).join(" ") + ": " + supplies.join(", ")
      end
     end
     #puts seas + "\n" + "\t" + holz
